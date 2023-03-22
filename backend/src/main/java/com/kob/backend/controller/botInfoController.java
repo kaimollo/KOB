bd248c6562ef1996d613735f2ae79ff7595567dd
kaimollo <1102646214@qq.com>
2023-03-22 17:37:30 +0800

    2023-3-22最后提交

--- backend/src/main/java/com/kob/backend/controller/botInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/botInfoController.java
@@ -3,18 +3,19 @@ package com.kob.backend.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pk")
 public class botInfoController {
     @RequestMapping("/getBotInfo")
-    List<String> getBotInfo(){
-        List<String> list =new LinkedList<>();
-        list.add("sword");
-        list.add("trigger");
-        list.add("apple");
-        return list;
+    Map<String,String> getBotInfo(){
+        Map<String,String> amp =new HashMap<>();
+        amp.put("age","18");
+        amp.put("name", "lihua");
+        return amp;
      }
 }
